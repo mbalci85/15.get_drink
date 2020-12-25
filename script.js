bab9bdef56bc5645f93ead11e87402b970e7ea88
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2020-12-25 23:12:35 +0000

    algorithm added in script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+//calculates number of bottle can be bought
+const calcBottles = (totalMoney, drinkPrice) => {
+	return Math.floor(totalMoney / drinkPrice);
+};
+
+// console.log(calcBottles(100, 3));
+// console.log(calcBottles(10, 6));
+
+//amount of change
+const calcChange = (totalMoney, drinkPrice) => {
+	return `Your change is ${totalMoney % drinkPrice} dollar(s)`;
+};
+
+// console.log(calcChange(50, 7));
+// console.log(calcChange(68, 13));
+
+const getDrink = (totalMoney, drinkPrice) => {
+	return `
+    -"Hello Master, I have $${totalMoney} and drink's price is $${drinkPrice}"
+    
+    -Leave House
+    -Turn Left
+    -Go to the market
+    -Buy ${calcBottles(totalMoney, drinkPrice)} bottles of drink
+    -Pay for drink
+    -Leave market
+    -Come back to the house
+    
+    -"Hello Master, here is your $${calcChange(
+			totalMoney,
+			drinkPrice,
+		)} change"`;
+};
+
+console.log(getDrink(1245, 27));
